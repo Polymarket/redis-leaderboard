8ec28dde2cd0f95de13e4592b2b3b60bfff1286f
cristiangpr <chivoblack@DESKTOP-0K5H7QO.localdomain>
2021-02-03 12:23:49 -0600

    build script

--- lib/getLeaderboardData.d.ts
+++
@@ -1,7 +0,0 @@
-import "cross-fetch/polyfill";
-/**
- * @function getLeaderboardData - fetches market data and calculates top ten traders for a given market address
- * @param {string} marketMakerAddress - the market address
- */
-export declare const getLeaderboardData: (marketMakerAddress: string) => Promise<import("./interfaces").BoardData>;
-export default getLeaderboardData;
